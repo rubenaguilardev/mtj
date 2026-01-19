ee1e6af76de02fa634cf407db8c067ff5062b09f
Ruben Aguilar <ragui56@wgu.edu>
2026-01-18 23:36:23 -0800

    add data, render entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Entry from "./components/Entry"
 import Header from "./components/Header"
+import data from './data/data'
 
 const App = () => {
+  console.log(data)
+  const entryElements = data.map(entry => (
+    <Entry 
+      key={entry.id}
+      image={entry.img}
+      title={entry.title}
+      country={entry.country}
+      mapLink={entry.googleMapsLink}
+      dates={entry.dates}
+      text={entry.text}
+    />
+  ))
+
   return (
     <div className="min-h-screen overflow-hidden max-w-136.25">
       <Header />
-      <Entry />
+      {entryElements}
     </div>
     
   )
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,11 +1,11 @@
-const Entry = () => {
+const Entry = (props) => {
     return (
         <section className="overflow-hidden p-10">
-            <div className="flex gap-5 items-center">
+            <div className="flex gap-5 items-center pb-10 border-b border-[#F5F5F5]">
                 <div className="shrink-0" >
                     <img 
-                        src="https://scrimba.com/links/travel-journal-japan-image-url" 
-                        alt="photo of mount fuji" 
+                        src={props.image.src}
+                        alt={props.image.alt}
                         className="w-31.25 h-42 object-cover rounded-md"
                     />
                 </div>
@@ -16,17 +16,17 @@ const Entry = () => {
                             alt="location icon"
                             className="w-2 h-3" 
                         />
-                        <span className="mr-2">JAPAN</span>
+                        <span className="mr-2">{props.country}</span>
                         <a 
                             className="underline underline-offset-3 text-[#918E9B]"
-                            href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+                            href={props.mapLink}
                         >
                             View on Google Maps
                         </a>
                     </div>
-                    <h1 className="text-[1.5625rem] font-bold mb-4">Mount Fuji</h1>
-                    <span className='inline-block font-bold mb-2'>12 Jan, 2023 - 24 Jan, 2023</span>
-                    <p className="leading-normal">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <h1 className="text-[1.5625rem] font-bold mb-3">{props.title}</h1>
+                    <span className='inline-block font-bold mb-2'>{props.dates}</span>
+                    <p className="leading-normal">{props.text}</p>
                 </div>
             </div>
            
